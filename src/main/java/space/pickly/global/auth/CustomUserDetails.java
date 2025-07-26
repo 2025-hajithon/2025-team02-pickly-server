15e82b89e96807f91215e397706b9d5f982a7746
Jaehyun Ahn <desktop3711@gmail.com>
2025-07-26 20:34:46 +0900

    chore: JWT 필터 구현

---
+++ src/main/java/space/pickly/global/auth/CustomUserDetails.java
@@ -0,0 +1,53 @@
+package space.pickly.global.auth;
+
+import static space.pickly.global.constant.SecurityConstant.*;
+
+import java.util.Collection;
+import java.util.Collections;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Getter
+@RequiredArgsConstructor
+public class CustomUserDetails implements UserDetails {
+
+    private final Long userId;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singleton(new SimpleGrantedAuthority(DEFAULT_ROLE_NAME));
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return userId.toString();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
--- src/main/java/space/pickly/global/exception/ErrorCode.java
+++ src/main/java/space/pickly/global/exception/ErrorCode.java
@@ -12,8 +12,16 @@ public enum ErrorCode {
 
     // COMMON
     SERVER_ERROR(INTERNAL_SERVER_ERROR, "서버 오류가 발생했습니다."),
+
+    // AUTH
     AUTH_ACCESS_DENIED(UNAUTHORIZED, "접근 권한이 없습니다."),
     INVALID_REFRESH_TOKEN_USED(UNAUTHORIZED, "비정상적인 리프레시 토큰입니다."),
+    INVALID_JWT(UNAUTHORIZED, "유효하지 않은 JWT입니다."),
+    AUTH_NOT_PARSABLE(INTERNAL_SERVER_ERROR, "시큐리티 인증 정보를 파싱할 수 없습니다."),
+    AUTH_NOT_EXIST(INTERNAL_SERVER_ERROR, "시큐리티 인증 정보가 존재하지 않습니다."),
+
+    // USER
+    USER_NOT_FOUND(NOT_FOUND, "사용자를 찾을 수 없습니다."),
     ;
 
     private final HttpStatus status;

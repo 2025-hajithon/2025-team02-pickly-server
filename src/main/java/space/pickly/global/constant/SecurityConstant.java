2970d75ed0476fd47cbf3ad8a32449ddda5fdca0
Jaehyun Ahn <desktop3711@gmail.com>
2025-07-26 18:45:41 +0900

    chore: 시큐리티 세팅

---
+++ src/main/java/space/pickly/global/constant/SecurityConstant.java
@@ -0,0 +1,12 @@
+package space.pickly.global.constant;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class SecurityConstant {
+
+    public static final String JWT_ROLE_CLAIM_NAME = "role";
+    public static final String ACCESS_TOKEN_HEADER_PREFIX = "Bearer ";
+    public static final String REFRESH_TOKEN_HEADER = "refresh-token";
+}
